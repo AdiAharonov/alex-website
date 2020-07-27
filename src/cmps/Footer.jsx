c383557e8c621fd8d0504f92d6d0421cf3c205d0
AdiAharonov <adi.ahronov@gmail.com>
2020-07-27 19:32:01 +0300

    update

--- src/cmps/Footer.jsx
+++ src/cmps/Footer.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
-import logo from '../assets/images/logo-w.png'
+import React from 'react';
+import logo from '../assets/images/logo-w.png';
 
 export function Footer() {
-    return (
-        <section className="footer">
-            <img src={logo} className="footer-logo" alt=""/>
-            <div className="social-media">
-            <i className="fab fa-facebook-square"></i>
-            <i className="fab fa-youtube"></i>
-            </div>
-            
-        </section>
-    )
+  return (
+    <section className="footer">
+      <div className="top-left">
+      <i className="fas fa-phone-square contact"></i>
+      <i class="fas fa-envelope-square contact"></i>
+      <i class="fas fa-map-marker-alt contact"></i>
+      </div>
+      <div className="top-right">
+        <i className="fab fa-facebook-square social-media"></i>
+        <i className="fab fa-youtube social-media"></i>
+        <img src={logo} className="footer-logo" alt="" />
+      </div>
+      <div className="bottom">
+        <p>Made By Adi Aharonov © 2020</p>
+      </div>
+    </section>
+  );
 }
